1a5be85125261110b2b83c8da3f7441950b85326
Bruno <bruno@bj-macbook-pro.local>
2019-03-20 23:50:20 -0300

    Estilizando lista de repos

--- src/styles/global.js
+++ src/styles/global.js
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
     background: #9865E6;
     text-rendering: optimizeLegibility !important;
     -webkit-font-smoothing: antialiased !important;
+    font-family: sans-serif;
   }
 `;
 
